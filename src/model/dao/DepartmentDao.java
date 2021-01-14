1da8223e8bfc4f6bac80626f7fc4c8c335a9a360
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-14 20:19:40 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
-public interface DepartmentDao {
+import java.util.List;
 
+import model.entities.Department;
+
+public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	void findById(Integer id);
+	List<Department> findAll();
 }
